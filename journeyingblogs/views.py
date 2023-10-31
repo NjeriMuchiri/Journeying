fb5ae4c48b3df20ee112cca9cd89ebb835c6c9f2
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-10-31 20:21:58 +0300

    adding templates to our journeyingblogs

--- journeyingblogs/views.py
+++ journeyingblogs/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'my software engineering journey'},
+    {'id': 2, 'name': 'my graphic designing journey'},
+    {'id': 3, 'name': 'my entry job journey'},
+]
 #our homepage view
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'journeyingblogs/home.html', context)
 
 #our chamber method view 
 def chamber(request):
-    return render(request, 'chamber.html')
+    return render(request, 'journeyingblogs/chamber.html')
