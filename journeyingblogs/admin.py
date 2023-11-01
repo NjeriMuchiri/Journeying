39cee04367623562771ddfb38c4da7ca127303d9
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-11-02 01:52:57 +0300

    rendering onto our template

--- journeyingblogs/admin.py
+++ journeyingblogs/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Chamber
+from .models import Chamber, Topic, Message
 
 # Register your models here.
 admin.site.register(Chamber)
+admin.site.register(Topic)
+admin.site.register(Message)
