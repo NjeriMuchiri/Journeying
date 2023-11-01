eb459843325e4d8ca4cac95a64a1520ece256481
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-11-02 01:34:37 +0300

    adding models

--- journeyingblogs/views.py
+++ journeyingblogs/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Chamber
 # Create your views here.
 
-chambers = [
-    {'id': 1, 'name': 'my software engineering journey'},
-    {'id': 2, 'name': 'my graphic designing journey'},
-    {'id': 3, 'name': 'my entry job journey'},
-]
+# chambers = [
+#     {'id': 1, 'name': 'my software engineering journey'},
+#     {'id': 2, 'name': 'my graphic designing journey'},
+#     {'id': 3, 'name': 'my entry job journey'},
+# ]
 #our homepage view
 def home(request):
+    chambers = Chamber.objects.all()
     context = {'chambers': chambers}
     return render(request, 'journeyingblogs/home.html', context)
 
 #our chamber method view 
 def chamber(request, pk):
-    return render(request, 'journeyingblogs/chamber.html')
+    chamber = Chamber.objects.get(id=pk)
+    context = {'chamber': chamber}
+    return render(request, 'journeyingblogs/chamber.html', context)
